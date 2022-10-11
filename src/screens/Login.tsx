d20d0b40d7520b1a3b19b78fc1f29bc7b28a656f
RicardoTorresits <ricardo.torresits@gmail.com>
2022-10-11 18:25:14 -0500

    maquetado login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -3,7 +3,38 @@ import React from 'react'
 export const Login = () => {
   return (
     <div>
-        Login
+        <div>
+            <img/>
+            <h1>
+                Wolves Points
+            </h1>
+        </div>
+        <div>
+            <div>
+                <div>
+
+                </div>
+                <div>
+                    <div>
+                        <label>
+                            correo:
+                        </label>
+                        <input>
+                        </input>
+                    </div>
+                    <div>
+                        <label>
+                            Contraseña:
+                        </label>
+                        <input>
+                        </input>
+                    </div>
+                </div>
+            </div>
+            <button>
+                Ingresar
+            </button>
+        </div>
     </div>
   )
 }
